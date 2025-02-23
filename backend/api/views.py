fac7068b3e76257bc551f8f120a9cc98bd0bb192
Martin Odongo <martinwere14@gmail.com>
2025-02-24 02:50:17 +0300

    did importations in the views.py file tdjango.shortcuts,django.contrib.auth.models,rest_framework ,serilizers and restframework.permissions

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
-from django.contrib
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated,AllowAny
 
-# Create your views here.
+# Create your views here.`
