71b457dc2b1c747f0395ce0383fe4d660037f0ca
Martin Odongo <martinwere14@gmail.com>
2025-02-23 15:37:36 +0300

    added corsheader , easiy since its from the same origin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,6 +52,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework',
+    'corsheaders'
+
+
 ]
 
 MIDDLEWARE = [
