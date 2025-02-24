8e0a4ce2ad676069854555095d122de6a3e33ab7
Martin Odongo <martinwere14@gmail.com>
2025-02-24 03:40:13 +0300

    created the different paths for the url patterns

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,4 +6,5 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/user/register/", CreateUserView.as_view(),name="register"),
 ]
